6e5b04ff7c0793502d029dfe0569e140124fefa5
Rami Khalil <the.rami.khalil@gmail.com>
2025-03-15 02:27:01 +0200

    risc0-build 3.0

--- build/risczero/build.rs
+++ build/risczero/build.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use risc0_build::GuestOptions;
-
 fn main() {
     if cfg!(feature = "rebuild-fpvm") {
         let build_opts = {
