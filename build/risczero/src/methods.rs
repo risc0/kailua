442562452de4163f63a2756726de75849f6d9ae4
Rami Khalil <the.rami.khalil@gmail.com>
2025-04-09 16:33:53 +0200

    rebake v2 elf

--- build/risczero/src/methods.rs
+++ build/risczero/src/methods.rs
@@ -1,3 +1,3 @@
-pub const KAILUA_FPVM_ELF: &[u8] = include_bytes!("./kailua-fpvm");
-pub const KAILUA_FPVM_PATH: &str = "./kailua-fpvm";
+pub const KAILUA_FPVM_ELF: &[u8] = include_bytes!("./kailua-fpvm.bin");
+pub const KAILUA_FPVM_PATH: &str = "./kailua-fpvm.bin";
 pub const KAILUA_FPVM_ID: [u32; 8] = [3960594314, 2174009817, 1994379181, 1026786251, 2281285287, 2891745598, 1631886767, 4000636813];
